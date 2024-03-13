8db9ccf09c61fd2876d2d8b784ba6fe71d32a544
Vander Gerald <vander.gerald@ui.ac.id>
2024-03-13 20:10:37 +0700

    [Refactor] changed StudentService again bya adding another @Query annotated method in the repository and calling that method to optimize the speed of findStudentWithHighestGpa()

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * @author muhammad.khadafi
  */
@@ -12,4 +14,7 @@ import org.springframework.stereotype.Repository;
 public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query("SELECT string_agg(student.name, ', ') FROM Student student")
     String getJoinedStudentNames();
+
+    @Query("SELECT student FROM Student student ORDER BY student.gpa DESC LIMIT 1")
+    Optional<Student> findHighestGpaStudent();
 }
